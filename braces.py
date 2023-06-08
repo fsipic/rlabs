21c52f2cbb7fc80cc26d6601e57be5d8fbf86c4e
Fran <fran.sipic@gmail.com>
2023-06-08 19:42:22 +0200

    refactoring

--- braces.py
+++ braces.py
@@ -1,21 +1,26 @@
-def check_brackets(text):
+def check_braces(text):
     stack = []
-    brackets = {"(": ")", "[": "]", "{": "}"}
+    braces = {"(": ")", "[": "]", "{": "}"}
     for char in text:
-        if char in brackets:
+        if char in braces:
             stack.append(char)
-        elif char in brackets.values():
-            if not stack or char != brackets[stack.pop()]:
+        elif char in braces.values():
+            if not stack or char != braces[stack.pop()]:
                 return False
     return not stack
 
 
 def main():
-    text = input("Input text: ")
-    if check_brackets(text):
-        print("Braces are balanced")
-    else:
-        print("Braces are not balanced")
+    file_name = input("Enter filename: ")
+    try:
+        with open(file_name, "r") as file:
+            text = file.read()
+            if check_braces(text):
+                print("Braces are balanced")
+            else:
+                print("Braces are not balanced")
+    except IOError as error:
+        print(f"An error occurred while opening the file: {error}")
 
 
 if __name__ == "__main__":
